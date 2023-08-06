50330d7a785af22c31d7a919e7eed664a571f0eb
YaaKhrys <chrisowusu003@gmail.com>
2023-08-06 21:19:05 +0000

    Started on basic CSS for Player & Computer scores

---
+++ index.js
@@ -0,0 +1,21 @@
+/** let playerChoice = "";
+
+let playerChoiceContainer = document.querySelector("#player-choice-container");
+
+playerChoiceContainer.addEventListener("click"), handlePlayerChoice
+
+
+/** let shapes = ["Rock", "Paper", "Scissors"]; // The shape variable containing the options the players can choose.
+
+
+// The Computer random choice of shape selection function
+function getComputerChoice (shapes) {
+
+    return shapes[Math.floor(Math.random()*shapes.length)];
+}
+
+console.log(getComputerChoice(shapes));
+
+// The Player selection of Shape
+
+let playerInput = ["Rock", "Paper", "Scissors"];
