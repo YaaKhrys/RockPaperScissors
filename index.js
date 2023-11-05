f4b47032d93d80fac34cacec70f74fb81b0d8b87
YaaKhrys <chrisowusu003@gmail.com>
2023-11-05 16:31:54 +0000

    PLayer moves stacked with z-index - Design done

--- index.js
+++ index.js
@@ -18,4 +18,39 @@ console.log(getComputerChoice(shapes));
 
 // The Player selection of Shape
 
-let playerInput = ["Rock", "Paper", "Scissors"]; **/
+let playerInput = ["Rock", "Paper", "Scissors"]; 
+
+
+// Varaibles to store the User and Computer moves (choices)
+let userMove;
+let computerMove;
+
+
+// Setting Up User Move Selection
+
+// Linking User move options div elements using their IDs
+const rockChoice = docuemnt.getElementById("rockIcon")
+const paperChoice = docuemnt.getElementById("paperIcon")
+const scissorsChoice = docuemnt.getElementById("scissorsIcon")
+
+
+// Adding event listeners to the User move options div elements
+rockChoice.addEventListener("click", () => userChooses("Rock"));
+paperChoice.addEventListener("click", () => userChooses("Paper"));
+scissorsChoice.addEventListener("click", () => userChooses("Scissors"));
+
+
+// Generating Cmputer's Moves (Choices)
+
+//Array of possible Computer choices
+const computerMove = ["Rock", "Paper" "Scissors"];
+
+// Generate a random index for computerMove
+const randomIndex =  Math.floor(Math.random()*computerMove.length);
+
+// Get the computer's choice using the random index
+const computerMove = computerMove[randomIndex];
+
+
+**/
+
