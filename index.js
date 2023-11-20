734c781965b1ba13304539aad6de9e1e23bf38c7
YaaKhrys <chrisowusu003@gmail.com>
2023-11-20 01:21:41 +0000

    Setup the varibles and References to HTML elements

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-/** let playerChoice = "";
-
-let playerChoiceContainer = document.querySelector("#player-choice-container");
-
-playerChoiceContainer.addEventListener("click"), handlePlayerChoice
-
-
-/** let shapes = ["Rock", "Paper", "Scissors"]; // The shape variable containing the options the players can choose.
-
-
-// The Computer random choice of shape selection function
-function getComputerChoice (shapes) {
-
-    return shapes[Math.floor(Math.random()*shapes.length)];
-}
-
-console.log(getComputerChoice(shapes));
-
-// The Player selection of Shape
-
-let playerInput = ["Rock", "Paper", "Scissors"]; 
+// SetUp Variables
+let userMove = "";
+let computerMove = "";
+let userScore = 0;   // Varaibles to track scores
+let computerScore = 0;  // Varaibles to track scores
+let playsINRound = 0;
+let round = 1;
 
 
-// Varaibles to store the User and Computer moves (choices)
-let userMove;
-let computerMove;
 
-
-// Setting Up User Move Selection
-
-// Linking User move options div elements using their IDs
-const rockChoice = docuemnt.getElementById("rockIcon")
-const paperChoice = docuemnt.getElementById("paperIcon")
-const scissorsChoice = docuemnt.getElementById("scissorsIcon")
+// References to HTML elements - Linking to IDs
+const rockChoice = docuemnt.getElementById("rockIcon"); // Icon for the User rock move
+const paperChoice = docuemnt.getElementById("paperIcon"); // Icon for the User paper move
+const scissorsChoice = docuemnt.getElementById("scissorsIcon"); // Icon for the User scissors move
+const playGame = docuemnt.getElementById("playIcon"); // Icon to play/start game
+const pauseGame = docuemnt.getElementById("pauseIcon"); // Icon to pause game
+const resetGame = docuemnt.getElementById("resetIcon"); // Icon to reset game
+const userMoveDisplay = docuemnt.getElementById("userMoveDisplay"); // Container that display's the user's moves
+const computerMoveDisplay = docuemnt.getElementById("computerMoveDisplay"); // Container that display's the computer's moves
+const userScoreDisplay = docuemnt.getElementById("userScoreDisplay"); // Display area for the user's scores
+const computerScoreDisplay = docuemnt.getElementById("computerScoreDisplay"); // Display area for the computer's scores
 
 
 // Adding event listeners to the User move options div elements
-rockChoice.addEventListener("click", () => userChooses("Rock"));
-paperChoice.addEventListener("click", () => userChooses("Paper"));
-scissorsChoice.addEventListener("click", () => userChooses("Scissors"));
+rockChoice.addEventListener("click", function () {userChooses("Rock");});
+paperChoice.addEventListener("click", function () {userChooses("Paper");});
+scissorsChoice.addEventListener("click", function () {userChooses("Scissors");});
+
+
+// Function to handle the user's move choice
+function userChooses (move) {
+
+    //Update's the user's move variable
+    userChooses = move;
+
+    // Display User's move ();
+    displayUserMove ();
+}
+
+
 
 
 // Generating Cmputer's Moves (Choices)
 
-//Array of possible Computer choices
-const computerMove = ["Rock", "Paper" "Scissors"];
+function generateComputerMove () {
 
-// Generate a random index for computerMove
-const randomIndex =  Math.floor(Math.random()*computerMove.length);
+    //Array of possible Computer choices
+    const computerMoves = ["Rock", "Paper" "Scissors"];
 
-// Get the computer's choice using the random index
-const computerMove = computerMove[randomIndex];
+    // Generate a random index for computerMove
+    const randomIndex =  Math.floor(Math.random()*computerMoves.length);
+
+    computerMove = computerMoves[randomIdex];
+
+    displayBothMoves();
+}
 
 
-**/
-
+// Function to display both player's moves after shuffling
